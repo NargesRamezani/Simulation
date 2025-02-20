75ffcf891efd7431885c3c4ebaa86388ea175cb9
NargesRamezani <nargessramezaiy>
2025-02-21 01:01:09 +0330

    add random algorithm

--- exercise1/Sierpinski-random.py
+++ exercise1/Sierpinski-random.py
@@ -19,3 +19,22 @@ def f3 (x ,y):
 
 functions=[f1 , f2 , f3]
 points=[]
+
+
+
+for _ in range(10000):
+    x= random.uniform(0, 2)
+    y=random.uniform(0, 2)
+    
+    for _ in range(20):
+        func=random.choice(functions)
+        x , y = func(x , y)
+    points.append((x,y))
+
+x_value , y_value =zip(*points)
+
+plt.scatter(x_value, y_value, s=0.1, color='purple')
+plt.xlim(0, 2)
+plt.ylim(0, 2)
+plt.gca().set_aspect('equal')
+plt.show()
