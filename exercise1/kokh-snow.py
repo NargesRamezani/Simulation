9870a0710d6b8de79f707db4e61235eb8b78310b
NargesRamezani <nargessramezaiy>
2025-02-23 22:45:50 +0330

    Add final edit

--- exercise1/kokh-snow.py
+++ exercise1/kokh-snow.py
@@ -1,52 +1,60 @@
+
+
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-x , y =0 , 0
-x1 , y1 = 3 , 0
-n=12
-
 def rot(theta, point, origin=(0, 0)):
+    
     x, y = point
     ox, oy = origin
     x_new = ox + (x - ox) * np.cos(theta) - (y - oy) * np.sin(theta)
     y_new = oy + (x - ox) * np.sin(theta) + (y - oy) * np.cos(theta)
-    return (x_new, y_new)
+    return x_new, y_new
 
-def kokh (x , y , x1 , y1 , n):
-    if n==0:
-        return[(x,y) , (x1 , y1) ]
+def tp(x, y, dx, dy):
     
-    dx = (x1 - x) / 3
-    dy = (y1 - y) / 3
+    return x + dx, y + dy
+
+def koch(x, y, x1, y1, n):
     
-    
-    xx = x + dx
-    yy = y + dy
-    Xx = x1 - dx
-    Yy = y1 - dy
-    
+    if n == 0:
+        return [(x, y), (x1, y1)]
 
     theta = np.pi / 3
+    dx = (x1 - x) / 3
+    dy = (y1 - y) / 3
 
-    x_mid, y_mid = rot(theta, (Xx, Yy), (xx, yy))
+
+    p1 = (x, y)
+    p2 = tp(x, y, dx, dy)
+    p3 = tp(*p2, dx, dy)  
+    
+
+    px, py = p2
+    p4 = rot(theta, (px + dx, py + dy), p2)  
+    
+    p5 = (x1, y1)
 
     
-    points=[]
+    points = koch(*p1, *p2, n - 1)
+    points += koch(*p2, *p4, n - 1)[1:]
+    points += koch(*p4, *p3, n - 1)[1:]
+    points += koch(*p3, *p5, n - 1)[1:]
 
-    #points = [(x , y),(xx , yy) , (x_mid , y_mid), (Xx , Yy), (x1 , y1)]
-    points.extend(kokh(x,y , xx , yy , n-1))
-    points.extend(kokh(xx,yy , x_mid , y_mid , n-1))
-    points.extend(kokh( x_mid , y_mid ,Xx,Yy , n-1))
-    points.extend(kokh( Xx,Yy , x1 ,y1, n-1))
     return points
 
-points = kokh(x , y ,x1, y1 , n)
-xval , yval = zip(*points)
+
+x, y = 0, 0
+x1, y1 = 3, 0
+n = 8
 
 
-plt.plot(xval, yval, color="blue", linestyle="-", label="Left Part")
+points = koch(x, y, x1, y1, n)
+xval, yval = zip(*points)
 
-plt.axis('equal')  
-plt.title(f"Kokh snowflake (n={n})")
+
+plt.figure(figsize=(8, 6))
+plt.plot(xval, yval, color="blue", linestyle="-", label="Koch Snowflake")
+plt.axis('equal')
+plt.title(f"Koch Curve (n={n})")
 plt.show()
