9f1681fa585b06bfed8a33647021a257ea3bb65c
NargesRamezani <nargessramezaiy>
2025-03-19 04:32:46 +0330

    Renormalization Group

---
+++ exercise3/9-4.py
@@ -0,0 +1,58 @@
+import numpy as np
+import random
+from scipy.optimize import fsolve
+import matplotlib.pyplot as plt
+
+L = 10
+P=np.arange(0.5, 0.7, 0.001).tolist()
+
+result={}
+
+
+for p in P:
+    grid = np.zeros((L, L), dtype=int)
+    for i in range(L):
+        for j in range(L):
+            if random.uniform(0, 1) < p:
+                grid[i, j] = 1
+
+
+
+    #print(grid)
+
+    new_L = L // 2
+    new_grid = np.zeros((new_L, new_L), dtype=int)
+
+
+    for i in range(0, L, 2):
+        for j in range(0, L, 2):
+            subgrid = grid[i:i+2, j:j+2]  
+            count = np.sum(subgrid)  
+
+            
+            if count == 4:  
+                new_grid[i//2, j//2] = 1
+            elif count == 3:  
+                new_grid[i//2, j//2] = 1
+            elif count == 2:
+                if np.array_equal(subgrid, [[1, 1], [0, 0]]) or np.array_equal(subgrid, [[0, 0], [1, 1]]):
+                    new_grid[i//2, j//2] = 1  
+
+
+    #print(new_grid)
+
+
+    p_prime = np.sum(new_grid) / (new_L * new_L)
+
+    p_calculated = round((p**4 + (4 * p**3 * (1 - p)) + (2 * p**2 * (1 - p)**2)) , 3)
+    p = round(p , 3)
+    result[p] = (round(p_calculated, 3), round(float(p_prime), 3))
+
+fixed_point = None
+for p, (p_calculated, p_prime) in result.items():
+    if abs(p - p_calculated) < 0.000001:  
+        fixed_point = p
+        break
+
+print("fixed point: ", fixed_point)
+#print(result)
